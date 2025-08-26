6800ef54904c2c19dce905eaba3fdf33045d509b
Gustavo <gustavotoyotadev@gmail.com>
2025-08-26 13:37:58 -0300

    banner e introducao da index

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+const sliderImagens = ["../src/assets/imagens/slider1.gif", "../src/assets/imagens/slider2.jpg", "../src/assets/imagens/slider3.gif"]
+let mostrarEstatica = false;
+const slider = document.querySelector("#slide")
+let i = 0
+function trocarSlide(){
+    if(i >= sliderImagens.length)
+    {
+        i = 0
+    }
+    if(mostrarEstatica) {
+        slider.src = "../src/assets/imagens/estatica.gif"
+        mostrarEstatica = false
+        setTimeout(trocarSlide, 1000)
+
+    }
+    else{
+        slider.src = sliderImagens[i]
+        mostrarEstatica = true
+        i++
+        setTimeout(trocarSlide, 3000)
+
+    }
+}
+trocarSlide()
