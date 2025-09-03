d4db73292a282c76bf9b3f31ed7f69cb7fff238f
jamaral33 <jpgamedev07@gmail.com>
2025-09-03 10:36:52 -0300

    Header responsivo

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,20 @@
-const sliderImagens = ["./src/assets/imagens/slider1.gif", "./src/assets/imagens/slider2.jpg", "./src/assets/imagens/slider3.gif"]
+const sliderImagens = {
+    'index' : 
+    ["./src/assets/imagens/slider1.gif", "./src/assets/imagens/slider2.jpg", "./src/assets/imagens/slider3.gif"],
+    'InfoSaw1' :
+    ["./src/assets/imagens/GordonTape(saw1).gif", "./src/assets/imagens/AdamScreaming(saw1).jpg", "./src/assets/imagens/AmandaBearTrap(saw1).gif"]
+}
+let pagina = window.location.pathname.replace("/ProjetoSAW/", "")
+pagina = pagina.replace(".html", "")
+console.log(pagina)
+
 let mostrarEstatica = false;
-const slider = document.querySelector("#slide")
+const slider = document.querySelector(".slide")
+
 let i = 0
+
 function trocarSlide(){
-    if(i >= sliderImagens.length)
+    if(i >= sliderImagens[pagina].length)
     {
         i = 0
     }
@@ -11,11 +22,13 @@ function trocarSlide(){
         slider.src = "./src/assets/imagens/estatica.gif"
         mostrarEstatica = false
         setTimeout(trocarSlide, 1000)
-
     }
     else{
-        slider.src = sliderImagens[i]
+        slider.src = sliderImagens[pagina][i]
+        console.log(sliderImagens[pagina][i])
         mostrarEstatica = true
+        console.log(slider.src)
+
         i++
         setTimeout(trocarSlide, 3000)
 
@@ -34,3 +47,4 @@ function trocarSubMenu(x){
 function trocarMenu(){
     menu.classList.toggle('close')
 }
+console.log(sliderImagens[pagina])
