303640e56a7ab1073485530307ba55afcc429efd
Gustavo <gustavotoyotadev@gmail.com>
2025-09-05 10:39:43 -0300

    resolve  bug s com o o slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,25 @@
-const sliderImagens = ["./src/assets/imagens/slider1.gif", "./src/assets/imagens/slider2.jpg", "./src/assets/imagens/slider3.gif"]
+const sliderImagens = {
+    'index' : 
+    ["./src/assets/imagens/slider1.gif", "./src/assets/imagens/slider2.jpg", "./src/assets/imagens/slider3.gif"],
+    'InfoSaw1' :
+    ["./src/assets/imagens/GordonTape(saw1).gif", "./src/assets/imagens/AdamScreaming(saw1).jpg", "./src/assets/imagens/AmandaBearTrap(saw1).gif"]
+}
+let pagina = window.location.pathname.replace("/ProjetoSAW/", "")
+pagina = pagina.replace(/\//g, "" )
+console.log(pagina)
+pagina = pagina.replace(".html", "")
+console.log(pagina)
+
 let mostrarEstatica = false;
 const slider = document.querySelector("#slide")
+const slider2 = document.querySelector("#slide2")
+
+
 let i = 0
+
 function trocarSlide(){
-    if(i >= sliderImagens.length)
+    console.log(sliderImagens[pagina])
+    if(i >= sliderImagens[pagina].length)
     {
         i = 0
     }
@@ -11,11 +27,14 @@ function trocarSlide(){
         slider.src = "./src/assets/imagens/estatica.gif"
         mostrarEstatica = false
         setTimeout(trocarSlide, 1000)
-
     }
     else{
-        slider.src = sliderImagens[i]
+        console.log(sliderImagens[pagina][i])
+        slider.src = sliderImagens[pagina][i]
+        console.log(sliderImagens[pagina][i])
         mostrarEstatica = true
+        console.log(slider.src)
+
         i++
         setTimeout(trocarSlide, 3000)
 
@@ -34,3 +53,4 @@ function trocarSubMenu(x){
 function trocarMenu(){
     menu.classList.toggle('close')
 }
+console.log(sliderImagens[pagina])
