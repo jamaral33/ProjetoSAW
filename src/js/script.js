78834ab1fd83d83d4d12fc5881d5193f3d4bbe24
Gustavo <gustavotoyotadev@gmail.com>
2025-09-08 12:15:12 -0300

    Comecando o saw final e arrumando o javascript

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,25 @@
 const sliderImagens = {
     'index' : 
     ["./src/assets/imagens/slider1.gif", "./src/assets/imagens/slider2.jpg", "./src/assets/imagens/slider3.gif"],
+    
     'InfoSaw1' :
-    ["./src/assets/imagens/GordonTape(saw1).gif", "./src/assets/imagens/AdamScreaming(saw1).jpg", "./src/assets/imagens/AmandaBearTrap(saw1).gif"]
+    ["./src/assets/imagens/GordonTape(saw1).gif", "./src/assets/imagens/AdamScreaming(saw1).jpg", "./src/assets/imagens/AmandaBearTrap(saw1).gif"],
+
+    'InfoSaw3':
+    ["./src/assets/imagens/saw3Slide1.gif", "./src/assets/imagens/slider1.gif", "./src/assets/imagens/saw3Slide2.gif"]
 }
+
 let pagina = window.location.pathname.replace("/ProjetoSAW/", "")
 pagina = pagina.replace(/\//g, "" )
 console.log(pagina)
 pagina = pagina.replace(".html", "")
 console.log(pagina)
 
+
 let mostrarEstatica = false;
 const slider = document.querySelector("#slide")
 const slider2 = document.querySelector("#slide2")
-
+console.log(slider2)
 
 let i = 0
 
@@ -25,12 +31,14 @@ function trocarSlide(){
     }
     if(mostrarEstatica) {
         slider.src = "./src/assets/imagens/estatica.gif"
+        slider2.src = "./src/assets/imagens/estatica.gif"
         mostrarEstatica = false
         setTimeout(trocarSlide, 1000)
     }
     else{
         console.log(sliderImagens[pagina][i])
         slider.src = sliderImagens[pagina][i]
+        slider2.src = sliderImagens[pagina][i]
         console.log(sliderImagens[pagina][i])
         mostrarEstatica = true
         console.log(slider.src)
